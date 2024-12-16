e32a7a496440bca21e3df6fea61dd8fb17241d72
Alessandro Surace <zioalex@gmail.com>
2024-12-16 22:51:30 +0100

    Updates

---
+++ precache-manifest.131aa61c0e6ad7497f46c4cbc643c977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4883d5c04dd3ab356ea94a04232d134",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7d4acbc2dc5d5d433e1",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "f7d4acbc2dc5d5d433e1",
+    "url": "/static/js/main.1cea3f7e.chunk.js"
+  },
+  {
+    "revision": "e67b41604717847bfcd7",
+    "url": "/static/js/runtime~main.e0c0d350.js"
+  }
+]);
